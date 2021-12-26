81a79d2ae5c8f2548e64e03570ad916efa5467d6
EvgeniySh77 <evgen.sh77@mail.ru>
2021-12-26 19:36:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UserSurvey
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите ваш возраст: ");
+            var age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Вас зовут {name} и вам {age} (год, года, лет).");
+
+            Console.Write("Введите вашу дату рождения: ");
+            string birtdate = Console.ReadLine();
+
+            Console.WriteLine("Вы родились - {0} ", birtdate);
+
+            Console.ReadKey();
+        }
+    }
+}
